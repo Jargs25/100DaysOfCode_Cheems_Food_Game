d1798c560b035866727ae7c4a9177c7080b47f2d
Jared <jazared_@hotmail.com>
2021-03-22 20:02:15 -0600

    Primer commit

---
+++ 100DaysOfCode_Cheems_Food_Game/frmGame.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace _100DaysOfCode_Cheems_Food_Game
+{
+    partial class frmGame
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbxBone = new System.Windows.Forms.PictureBox();
+            this.pbxBonus = new System.Windows.Forms.PictureBox();
+            this.pbxCheems = new System.Windows.Forms.PictureBox();
+            this.lblPuntos = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCPuntos = new System.Windows.Forms.Label();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.lblCTiempo = new System.Windows.Forms.Label();
+            this.tmrTiempo = new System.Windows.Forms.Timer(this.components);
+            this.lblGO = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBone)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBonus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCheems)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbxBone
+            // 
+            this.pbxBone.Location = new System.Drawing.Point(0, 30);
+            this.pbxBone.Name = "pbxBone";
+            this.pbxBone.Size = new System.Drawing.Size(99, 42);
+            this.pbxBone.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxBone.TabIndex = 0;
+            this.pbxBone.TabStop = false;
+            // 
+            // pbxBonus
+            // 
+            this.pbxBonus.Enabled = false;
+            this.pbxBonus.Location = new System.Drawing.Point(495, 300);
+            this.pbxBonus.Name = "pbxBonus";
+            this.pbxBonus.Size = new System.Drawing.Size(70, 60);
+            this.pbxBonus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxBonus.TabIndex = 0;
+            this.pbxBonus.TabStop = false;
+            this.pbxBonus.Visible = false;
+            // 
+            // pbxCheems
+            // 
+            this.pbxCheems.Location = new System.Drawing.Point(0, 257);
+            this.pbxCheems.Name = "pbxCheems";
+            this.pbxCheems.Size = new System.Drawing.Size(160, 103);
+            this.pbxCheems.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxCheems.TabIndex = 0;
+            this.pbxCheems.TabStop = false;
+            // 
+            // lblPuntos
+            // 
+            this.lblPuntos.AutoSize = true;
+            this.lblPuntos.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntos.Location = new System.Drawing.Point(6, 6);
+            this.lblPuntos.Name = "lblPuntos";
+            this.lblPuntos.Size = new System.Drawing.Size(70, 19);
+            this.lblPuntos.TabIndex = 1;
+            this.lblPuntos.Text = "Puntos:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblCTiempo);
+            this.panel1.Controls.Add(this.lblCPuntos);
+            this.panel1.Controls.Add(this.lblTiempo);
+            this.panel1.Controls.Add(this.lblGO);
+            this.panel1.Controls.Add(this.lblPuntos);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(704, 30);
+            this.panel1.TabIndex = 2;
+            // 
+            // lblCPuntos
+            // 
+            this.lblCPuntos.AutoSize = true;
+            this.lblCPuntos.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPuntos.Location = new System.Drawing.Point(82, 6);
+            this.lblCPuntos.Name = "lblCPuntos";
+            this.lblCPuntos.Size = new System.Drawing.Size(18, 19);
+            this.lblCPuntos.TabIndex = 1;
+            this.lblCPuntos.Text = "0";
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTiempo.Location = new System.Drawing.Point(563, 6);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(72, 19);
+            this.lblTiempo.TabIndex = 1;
+            this.lblTiempo.Text = "Tiempo:";
+            // 
+            // lblCTiempo
+            // 
+            this.lblCTiempo.AutoSize = true;
+            this.lblCTiempo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCTiempo.Location = new System.Drawing.Point(641, 6);
+            this.lblCTiempo.Name = "lblCTiempo";
+            this.lblCTiempo.Size = new System.Drawing.Size(51, 19);
+            this.lblCTiempo.TabIndex = 1;
+            this.lblCTiempo.Text = "00:00";
+            // 
+            // tmrTiempo
+            // 
+            this.tmrTiempo.Interval = 1000;
+            this.tmrTiempo.Tick += new System.EventHandler(this.tmrTiempo_Tick);
+            // 
+            // lblGO
+            // 
+            this.lblGO.AutoSize = true;
+            this.lblGO.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGO.Location = new System.Drawing.Point(285, 6);
+            this.lblGO.Name = "lblGO";
+            this.lblGO.Size = new System.Drawing.Size(106, 19);
+            this.lblGO.TabIndex = 1;
+            this.lblGO.Text = "GAME OVER";
+            this.lblGO.Visible = false;
+            // 
+            // frmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 361);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pbxCheems);
+            this.Controls.Add(this.pbxBonus);
+            this.Controls.Add(this.pbxBone);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cheems - Food Game";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmGame_FormClosing);
+            this.Load += new System.EventHandler(this.frmGame_Load);
+            this.Leave += new System.EventHandler(this.frmGame_Leave);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.frmGame_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBone)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBonus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCheems)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxBone;
+        private System.Windows.Forms.PictureBox pbxBonus;
+        private System.Windows.Forms.PictureBox pbxCheems;
+        private System.Windows.Forms.Label lblPuntos;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCTiempo;
+        private System.Windows.Forms.Label lblCPuntos;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Timer tmrTiempo;
+        private System.Windows.Forms.Label lblGO;
+    }
+}
+
---
+++ 100DaysOfCode_Cheems_Food_Game/frmGame.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace _100DaysOfCode_Cheems_Food_Game
+{
+    public partial class frmGame : Form
+    {
+        public string path;
+        public Thread trdFood;
+        public Thread trdBonus;
+        public delegate void DelBone(PictureBox pbx);
+        public delegate void DelBoneStart(PictureBox pbx, Size pnt);
+        public delegate void DelBoneImage(PictureBox pbx, string name);
+        public int contBonus = 0;
+        public int contMove = 0;
+        public string lastMove = "Right";
+        public int puntosFood = 1;
+        public int puntosBonus = 5;
+        public int puntos = 0;
+        public int tiempo = 50;
+
+        public int gravedad = 13;
+        Random rPositionX = new Random();
+
+        public frmGame()
+        {
+            InitializeComponent();
+        }
+
+        private void frmGame_Load(object sender, EventArgs e)
+        {
+            path = Application.StartupPath.Replace("bin\\Debug", "Imagenes\\");
+            NewImage(pbxBone, "Hueso.png");
+            NewImage(pbxBonus, "Pastelfresa.png");
+            NewImage(pbxCheems, "Right\\0.png");
+
+            trdFood = new Thread(new ThreadStart(() => StartDown(pbxBone)));
+            trdFood.Start();
+            trdBonus = new Thread(new ThreadStart(() => StartBonus(pbxBonus)));
+            trdBonus.Start();
+            Focus();
+            lblCTiempo.Text = "00:" + tiempo.ToString();
+            tmrTiempo.Start();
+        }
+
+        private void StartDown(PictureBox pbx)
+        {
+            int velocidad = 0, maximo = 0, aceleracion = 0, last = 0;
+            int locY = pbx.Location.Y;
+            ReStart(pbx, new Size(rPositionX.Next(0, 661), locY));
+            int tipoComida = 0;
+            int contTipoC = 0;
+
+            while (true)
+            {
+                velocidad = Convert.ToInt32(318 - (pbxBone.Location.Y + 42));
+                maximo = Convert.ToInt32(50 / gravedad);
+                aceleracion = (velocidad / gravedad);
+                last = 1;
+                while (pbx.Location.Y < 318 && pbx.Enabled)
+                {
+                    Thread.Sleep(velocidad > maximo ? velocidad : maximo);
+                    Down(pbx);
+                    if (maximo < velocidad)
+                    {
+                        velocidad = velocidad - aceleracion;
+                        aceleracion = velocidad / gravedad;
+                        if (aceleracion > 0) last = aceleracion;
+                        else aceleracion = last;
+                    }
+                }
+                if (pbx.Enabled)
+                {
+                    Thread.Sleep(500);
+                    if (pbx.Enabled)
+                    for (int i = 0; i < 7; i++)
+                    {
+                        if (pbx.Enabled)
+                        {
+                            Thread.Sleep(250);
+                            Show(pbx);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                
+                ReStart(pbx, new Size(rPositionX.Next(0, 604), locY));
+                
+                tipoComida = rPositionX.Next(0, 100);
+                if (tipoComida > 50 && contTipoC > 3)
+                {
+                    NewImage(pbx, "Pierna.png");
+                    contTipoC = 0;
+                    puntosFood = 3;
+                }
+                else
+                {
+                    NewImage(pbx, "Hueso.png");
+                    puntosFood = 1;
+                    contTipoC++;
+                }
+
+                contBonus++;
+                if (!pbx.Visible)
+                {
+                    Show(pbx);
+                }
+                if (!pbx.Enabled)
+                {
+                    Eaten(pbx);
+                }
+            }
+
+
+            //Thread.CurrentThread.Abort();
+        }
+        private void StartBonus(PictureBox pbx)
+        {
+            while (true)
+            {
+                if (contBonus == 4)
+                {
+                    Show(pbx);
+                    Eaten(pbx);
+                    puntosBonus = 5;
+                }
+                //else if (contBonus == 8)
+                //{
+                //    contBonus = 0;
+                //}
+
+                if (contBonus == 4)
+                {
+                    if (pbx.Enabled)
+                    {
+                        Thread.Sleep(1000);
+                        if (pbx.Enabled)
+                        for (int i = 0; i < 7; i++)
+                        {
+                            if (pbx.Enabled)
+                            {
+                                Thread.Sleep(250);
+                                Show(pbx);
+                            }
+                            else
+                            {
+                                Show(pbx);
+                                break;
+                            }
+                        }
+                    }
+                    contBonus = 0;
+                    ReStart(pbx, new Size(rPositionX.Next(0,621),pbx.Location.Y));
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Down(PictureBox pbx)
+        {
+            if (pbx.InvokeRequired)
+            {
+                DelBone delBone = new DelBone(Down);
+                pbx.Invoke(delBone, pbx);
+            }
+            else
+            {
+                pbx.Location = new Point(pbx.Location.X, pbx.Location.Y + 1);
+            }
+        }
+        private void Show(PictureBox pbx)
+        {
+            if (pbx.InvokeRequired)
+            {
+                DelBone delBone = new DelBone(Show);
+                pbx.Invoke(delBone, pbx);
+            }
+            else
+            {
+                if (pbx.Visible)
+                {
+                    pbx.Visible = false;
+                }
+                else
+                {
+                    pbx.Visible = true;
+                }
+            }
+        }
+        private void Eaten(PictureBox pbx)
+        {
+            if (pbx.InvokeRequired)
+            {
+                DelBone delBone = new DelBone(Eaten);
+                pbx.Invoke(delBone, pbx);
+            }
+            else
+            {
+                pbx.Enabled = true;
+            }
+        }
+        private void ReStart(PictureBox pbx, Size pnt)
+        {
+            if (pbx.InvokeRequired)
+            {
+                DelBoneStart delBone = new DelBoneStart(ReStart);
+                pbx.Invoke(delBone, new object[]{ pbx, pnt});
+            }
+            else
+            {
+                pbx.Location = new Point(pnt);
+            }
+        }
+        private void NewImage(PictureBox pbx, string name)
+        {
+            if (pbx.InvokeRequired)
+            {
+                DelBoneImage delBone = new DelBoneImage(NewImage);
+                pbx.Invoke(delBone, new object[] { pbx, name });
+            }
+            else
+            {
+                pbx.Image = Image.FromFile(path + name);
+            }
+        }
+
+        private void frmGame_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            trdFood.Abort();
+            trdBonus.Abort();
+        }
+        private void frmGame_Leave(object sender, EventArgs e)
+        {
+            Focus();
+        }
+
+        private void frmGame_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            int chemsLocX = 0;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    if (pbxCheems.Location.X < 540)
+                    {
+                        pbxCheems.Location = new Point(pbxCheems.Location.X + 20, pbxCheems.Location.Y);
+                        if (lastMove != Keys.Right.ToString()) contMove = 0;
+                        contMove++;
+                        if (contMove > 3) contMove = 0;
+                        NewImage(pbxCheems, "Right\\" + contMove + ".png");
+                        lastMove = Keys.Right.ToString();
+                        chemsLocX = pbxCheems.Location.X + 160;
+                    }
+                    break;
+                case Keys.Left:
+                    if (pbxCheems.Location.X > 0)
+                    {
+                        pbxCheems.Location = new Point(pbxCheems.Location.X - 20, pbxCheems.Location.Y);
+                        if (lastMove != Keys.Left.ToString()) contMove = 0;
+                        contMove++;
+                        if (contMove > 3) contMove = 0;
+                        NewImage(pbxCheems, "Left\\" + contMove + ".png");
+                        lastMove = Keys.Left.ToString();
+                        chemsLocX = pbxCheems.Location.X;
+                    }
+                    break;
+            }
+
+            if (chemsLocX > pbxBone.Location.X && chemsLocX < pbxBone.Location.X + 100 &&
+                pbxCheems.Location.Y < pbxBone.Location.Y && pbxBone.Enabled)
+            {
+                puntos = puntos + puntosFood;
+                pbxBone.Visible = false;
+                pbxBone.Enabled = false;
+                lblCPuntos.Text = puntos.ToString();
+            }
+            if (chemsLocX > pbxBonus.Location.X && chemsLocX < pbxBonus.Location.X + 70 &&
+                pbxCheems.Location.Y < pbxBonus.Location.Y && pbxBonus.Enabled)
+            {
+                puntos = puntos + puntosBonus;
+                pbxBonus.Visible = false;
+                pbxBonus.Enabled = false;
+                lblCPuntos.Text = puntos.ToString();
+                tiempo = tiempo + 5;
+                lblCTiempo.Text = "00:" + (tiempo > 9 ? tiempo.ToString() : "0" + tiempo.ToString());
+            }
+        }
+
+        private void tmrTiempo_Tick(object sender, EventArgs e)
+        {
+            tiempo--;
+            lblCTiempo.Text = "00:" + (tiempo > 9 ? tiempo.ToString() : "0" + tiempo.ToString());
+            if (tiempo <= 0)
+            {
+                trdFood.Abort();
+                trdBonus.Abort();
+                lblGO.Visible = true;
+                tmrTiempo.Stop();
+            }
+        }
+    }
+}
